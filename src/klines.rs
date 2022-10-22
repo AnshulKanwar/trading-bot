a1db8871a6e40838d63f30e0b9493a79b96b3d83
Anshul Kanwar <4n5hu1@protonmail.com>
2022-10-22 18:48:38 +0530

    modules created

---
+++ src/klines.rs
@@ -0,0 +1,44 @@
+use serde::Deserialize;
+#[derive(Deserialize)]
+pub struct _Kline(
+    i64,
+    String,
+    String,
+    String,
+    String,
+    String,
+    i64,
+    String,
+    u32,
+    String,
+    String,
+    String,
+);
+
+#[derive(Deserialize)]
+pub struct _Klines(pub Vec<_Kline>);
+
+#[derive(Debug)]
+pub struct Kline {
+    open_time: i64,
+    open_price: f32,
+    high_price: f32,
+    low_price: f32,
+    close_price: f32,
+    volume: f32,
+    close_time: i64,
+}
+
+impl Kline {
+    pub fn new(data: &_Kline) -> Self {
+        Kline {
+            open_time: data.0,
+            open_price: data.1.parse().unwrap(),
+            high_price: data.2.parse().unwrap(),
+            low_price: data.3.parse().unwrap(),
+            close_price: data.4.parse().unwrap(),
+            volume: data.5.parse().unwrap(), 
+            close_time: data.6,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,8 @@
-use serde::Deserialize;
+use crate::klines::{_Klines, Kline};
 
-#[derive(Deserialize)]
-struct _Kline(
-    i64,
-    String,
-    String,
-    String,
-    String,
-    String,
-    i64,
-    String,
-    u32,
-    String,
-    String,
-    String,
-);
+mod klines;
 
-#[derive(Deserialize)]
-struct _Klines(Vec<_Kline>);
-
-#[derive(Debug)]
-struct Kline {
-    open_time: i64,
-    open_price: f32,
-    high_price: f32,
-    low_price: f32,
-    close_price: f32,
-    volume: f32,
-    close_time: i64,
-}
-
-impl Kline {
-    fn new(data: &_Kline) -> Self {
-        Kline {
-            open_time: data.0,
-            open_price: data.1.parse().unwrap(),
-            high_price: data.2.parse().unwrap(),
-            low_price: data.3.parse().unwrap(),
-            close_price: data.4.parse().unwrap(),
-            volume: data.5.parse().unwrap(), 
-            close_time: data.6,
-        }
-    }
-}
-
-#[tokio::main]
-async fn main() {
+async fn get_data() -> _Klines {
     let client = reqwest::Client::new();
     let response = client
         .get("https://api.binance.com/api/v3/klines")
@@ -55,6 +12,13 @@ async fn main() {
         .unwrap();
 
     let data = response.json::<_Klines>().await.unwrap();
+
+    data
+}
+
+#[tokio::main]
+async fn main() {
+    let data = get_data().await;
 
     let klines: Vec<Kline> = data.0.iter().map(|kline| {
         Kline::new(kline)
