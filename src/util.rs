b927a167a17dcf569c1409474d315b035b7f2aa6
Anshul Kanwar <4n5hu1@protonmail.com>
2022-10-24 14:39:22 +0530

    Added tests

--- src/util.rs
+++ src/util.rs
@@ -3,10 +3,10 @@ use sha2::Sha256;
 
 type HmacSha256 = Hmac<Sha256>;
 
-pub fn get_signature(key: &str, query_str: &str) -> String {
+pub fn get_signature(key: &str, message: &str) -> String {
     let mut mac = HmacSha256::new_from_slice(key.as_bytes()).unwrap();
 
-    mac.update(query_str.as_bytes());
+    mac.update(message.as_bytes());
 
     let result = mac.finalize();
 
@@ -16,4 +16,17 @@ pub fn get_signature(key: &str, query_str: &str) -> String {
     let signature = hex::encode(bytes);
 
     signature
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello_world() {
+        let key = "my secret and secure key";
+        let message = "hello world";
+
+        assert_eq!(get_signature(key, message), "0b6594ce57238a3c2473b0dfb9d700398044f7c43246dce5303bbc4a79fb24d3")
+    }
 }
